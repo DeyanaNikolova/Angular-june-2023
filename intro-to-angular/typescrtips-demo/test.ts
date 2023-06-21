09da1cefe2820eab3cbed967e8971a00b80bc651
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-06-21 14:16:15 +0100

    Add inherit example

--- intro-to-angular/typescrtips-demo/test.ts
+++ intro-to-angular/typescrtips-demo/test.ts
@@ -1,6 +1,8 @@
 let isEven: boolean = false;
 let num: number = 4;
 let str: string = "";
+
+
 type User = {
     name: string;
     age: number;
@@ -35,3 +37,25 @@ user2List.forEach(({ firstName, id }) => {
     console.log(`${id}: ${firstName}`);
 });
 
+
+class Human {
+   speak(){
+    console.log("hello!");
+    
+   }
+}
+
+
+class Person extends Human{
+    private name: string;
+    age: number;
+    constructor(name: string, age: number){
+        super();
+        this.name = name;
+        this.age = age;
+    }
+}
+
+const personIvan = new Person("Ivan", 33);
+personIvan.age = 45;
+personIvan.speak();
