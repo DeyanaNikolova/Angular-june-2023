1c7ab4ed076d07085294a5717ef3c5fa23bf2d3c
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-09-25 17:43:21 +0100

    Add change detection strategy on push

--- dependancy-injection-rxjs-services/demo-rxjs-services/src/app/users-list/users-list.component.ts
+++ dependancy-injection-rxjs-services/demo-rxjs-services/src/app/users-list/users-list.component.ts
@@ -1,10 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
 import { User } from '../types/User';
+
 
 @Component({
   selector: 'app-users-list',
   templateUrl: './users-list.component.html',
-  styleUrls: ['./users-list.component.css']
+  styleUrls: ['./users-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UsersListComponent {
 @Input() users: User[] = [];
