76492f74bebc7afb96d8829dc334c142f7daa444
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-10-01 14:09:15 +0100

    Generate todo card

--- to-do-app/to-do-app/src/app/app.module.ts
+++ to-do-app/to-do-app/src/app/app.module.ts
@@ -5,12 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todos/todo-list/todo-list.component';
 import { HeaderComponent } from './header/header.component';
+import { PopupComponent } from './todos/popup/popup.component';
+import { TodoCardComponent } from './todos/todo-card/todo-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoListComponent,
-    HeaderComponent
+    HeaderComponent, 
+    PopupComponent, TodoCardComponent
   ],
   imports: [
     BrowserModule,
