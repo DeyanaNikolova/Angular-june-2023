ecd59210c09ec94b131ba2b0c9ef27740871d57f
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-10-01 12:36:34 +0100

    Generate popup component

--- to-do-app/to-do-app/src/app/app.module.ts
+++ to-do-app/to-do-app/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoListComponent } from './todos/todo-list/todo-list.component';
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
