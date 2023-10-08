7a48d700504ea26f9c1164404217aa596ff421ce
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-10-08 09:21:44 +0100

    Add theme interface

--- forum/src/app/api.service.ts
+++ forum/src/app/api.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment.development';
+import { Theme } from './types/Theme';
+import { Post } from './types/Post';
 
 @Injectable({
   providedIn: 'root'
@@ -12,13 +14,13 @@ export class ApiService {
   getThemes(){
     const { appUrl } = environment;
 
-  return this.http.get(`${appUrl}/themes`);
+  return this.http.get<Theme[]>(`${appUrl}/themes`);
   }
 
   getPosts(limit?: number){
     const { appUrl } = environment;
     const limitFilter = limit ? `?limit=${limit}` : '';
 
-  return  this.http.get(`${appUrl}/posts${limitFilter}`);
+  return  this.http.get<Post[]>(`${appUrl}/posts${limitFilter}`);
   }
 }
