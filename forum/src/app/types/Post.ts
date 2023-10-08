93f8ec17e4bc821e1eaf37e83280417cc7a51063
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-10-08 09:27:42 +0100

    Add post interface

---
+++ forum/src/app/types/Post.ts
@@ -0,0 +1,25 @@
+import { UserId } from "./Theme";
+
+export interface Post{
+likes: [];
+_id: string;
+text: string;
+userId: UserId;
+themeId: ThemeId;
+created_at: string;
+updatedAt: string;
+__v: number;
+}
+
+interface ThemeId  {
+    subscribers: string[];
+    posts: string[];
+    _id: string;
+    themeName: string;
+    userId: string;
+    created_at: string;
+    updatedAt: string;
+    __v: number;
+}
+  
+   
