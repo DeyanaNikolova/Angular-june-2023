079a4f5d91282d9cb7ca5cf50b97898c92ecead3
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-10-11 17:02:04 +0100

    Fetch users from jsonplaceholder

--- modules-and-routing/modules-routing/src/app/user/user.service.ts
+++ modules-and-routing/modules-routing/src/app/user/user.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../types/User';
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserService {
+export class UserService implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+  
+  }
+
+  fetchUsers(){
+   return this.http.get<User>('https://jsonplaceholder.typicode.com/users')
+  }
 }
