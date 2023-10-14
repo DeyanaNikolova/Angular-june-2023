6254705ea30c8f8604196093bcce6b583a35affe
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-10-14 17:02:23 +0100

    Add user details resolver logic

--- modules-and-routing/modules-routing/src/app/user/user.module.ts
+++ modules-and-routing/modules-routing/src/app/user/user.module.ts
@@ -3,23 +3,21 @@ import { CommonModule } from '@angular/common';
 import { UserListComponent } from './user-list/user-list.component';
 import { RouterModule } from '@angular/router';
 import { UserDetailsComponent } from './user-details/user-details.component';
-
-
-
+import { UserResolver } from './user-details/user-details.resolver';
 
 @NgModule({
-  declarations: [
-    UserListComponent
-  ],
+  declarations: [UserListComponent],
   imports: [
     CommonModule,
     RouterModule.forChild([
-      {path: 'user/list', component: UserListComponent},
-      {path: 'user/details/:id', component: UserDetailsComponent}
+      { path: 'user/list', component: UserListComponent },
+      {
+        path: 'user/details/:id',
+        resolve: { user: UserResolver },
+        component: UserDetailsComponent,
+      },
     ]),
   ],
-  exports: [
-    UserListComponent
-  ]
+  exports: [UserListComponent],
 })
-export class UserModule { }
+export class UserModule {}
