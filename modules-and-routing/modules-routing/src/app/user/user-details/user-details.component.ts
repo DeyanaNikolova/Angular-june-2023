a82393cced94ee35b516f7ed37d727ef05e1a315
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-10-14 16:31:26 +0100

    Add user-details component

---
+++ modules-and-routing/modules-routing/src/app/user/user-details/user-details.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-user-details',
+  templateUrl: './user-details.component.html',
+  styleUrls: ['./user-details.component.css']
+})
+export class UserDetailsComponent implements OnInit {
+
+
+  constructor(private activatedRoute: ActivatedRoute) {
+
+  console.log('snapshot.data ', this.activatedRoute.snapshot.data);
+
+  this.activatedRoute.params.subscribe((v)=> console.log('params.subscribe ', v));
+  }
+  ngOnInit(): void {
+    
+  }
+}
--- modules-and-routing/modules-routing/src/app/user/user.module.ts
+++ modules-and-routing/modules-routing/src/app/user/user.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserListComponent } from './user-list/user-list.component';
 import { RouterModule } from '@angular/router';
+import { UserDetailsComponent } from './user-details/user-details.component';
 
 
 
@@ -13,7 +14,8 @@ import { RouterModule } from '@angular/router';
   imports: [
     CommonModule,
     RouterModule.forChild([
-      {path: 'user/list', component: UserListComponent}
+      {path: 'user/list', component: UserListComponent},
+      {path: 'user/details/:id', component: UserDetailsComponent}
     ]),
   ],
   exports: [
