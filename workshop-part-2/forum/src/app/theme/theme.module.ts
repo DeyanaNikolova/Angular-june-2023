959db75e78b3be60d4b33425fe3774d894323f65
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-10-15 13:22:23 +0100

    Rename theme-routing-module

--- workshop-part-2/forum/src/app/theme/theme.module.ts
+++ workshop-part-2/forum/src/app/theme/theme.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NewThemeComponent } from './new-theme/new-theme.component';
 import { CurrentThemeComponent } from './current-theme/current-theme.component';
-import { ThemeRoutingModule } from './theme-reoting.module';
+import { ThemeRoutingModule } from './theme-routing.module';
 
 
 
