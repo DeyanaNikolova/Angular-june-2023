533e1cf7d56318aaf10bcadb1e570936ee835082
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-10-15 09:27:38 +0100

    Remove ngOnInit from user-details component

--- modules-and-routing/modules-routing/src/app/user/user-details/user-details.component.ts
+++ modules-and-routing/modules-routing/src/app/user/user-details/user-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 
@@ -7,16 +7,14 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './user-details.component.html',
   styleUrls: ['./user-details.component.css']
 })
-export class UserDetailsComponent implements OnInit {
+export class UserDetailsComponent {
 
 
   constructor(private activatedRoute: ActivatedRoute) {
-
+    // static way
   console.log('snapshot.data ', this.activatedRoute.snapshot.data['user']);
 
+  // dynamic way
   this.activatedRoute.params.subscribe((v)=> console.log('params.subscribe ', v));
-  }
-  ngOnInit(): void {
-   
   }
 }
