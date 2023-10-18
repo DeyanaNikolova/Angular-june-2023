ad82f3912f6ba1615198c3942285ea880ecc9950
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-10-18 15:39:58 +0100

    Add example on-mouse-enter

--- forms/forms/src/app/highlight-on-move.directive.ts
+++ forms/forms/src/app/highlight-on-move.directive.ts
@@ -8,8 +8,33 @@ export class HighlightOnMoveDirective implements OnInit {
 
   ngOnInit(): void {
     console.log(this.elRef);
+    // hardcoded
     // this.elRef.nativeElement.style.backgroundColor = 'yellow';
 
-    this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'red');
+    // using Render2
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'red');
+
+    this.renderer.listen(
+      this.elRef.nativeElement,
+      'mouseenter',
+      this.mouseEnterHandler.bind(this)
+    );
+
+    this.renderer.listen(
+      this.elRef.nativeElement,
+      'mouseleave',
+      this.mouseLeaveHandler.bind(this)
+    );
+  }
+
+  mouseEnterHandler(e: MouseEvent): void {
+    console.log('enter', e);
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'yellow');
+
+  }
+
+  mouseLeaveHandler(e: MouseEvent): void {
+    console.log('leave', e);
+     this.renderer.removeStyle(this.elRef.nativeElement, 'background-color');
   }
 }
