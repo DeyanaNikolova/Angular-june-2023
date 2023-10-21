00a5714061710ae403609ba6f558ddab4af3d37b
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-10-21 13:21:42 +0100

    Create my-structural directive

--- forms/forms/src/app/app.module.ts
+++ forms/forms/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { MyRouterLinkDirective } from './my-router-link.directive';
+import { MyStructuralDirective } from './my-structural.directive';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { MyRouterLinkDirective } from './my-router-link.directive';
     HomeComponent,
     LoginComponent,
     RegisterComponent,
-    MyRouterLinkDirective
+    MyRouterLinkDirective,
+    MyStructuralDirective
   ],
   imports: [
     BrowserModule,
---
+++ forms/forms/src/app/my-structural.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appMyStructuralDirective]'
+})
+export class MyStructuralDirective {
+
+  constructor() { }
+
+}
