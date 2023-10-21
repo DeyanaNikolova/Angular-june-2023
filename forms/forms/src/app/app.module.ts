7e9ca5940176b1535d47a4f263b370513a144e62
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-10-21 12:03:29 +0100

    Create home component

--- forms/forms/src/app/app.module.ts
+++ forms/forms/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HighlightOnMoveDirective } from './highlight-on-move.directive';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HighlightOnMoveDirective
+    HighlightOnMoveDirective,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
