b21ecb76660325fe7d3e24ad2fea559e4f7b5fe6
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-10-21 12:04:04 +0100

    Create login component

--- forms/forms/src/app/app.module.ts
+++ forms/forms/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HighlightOnMoveDirective } from './highlight-on-move.directive';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HighlightOnMoveDirective,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
