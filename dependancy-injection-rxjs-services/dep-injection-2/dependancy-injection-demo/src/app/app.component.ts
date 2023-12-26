98ea7b09456d506696e815dd446f430855bd9f29
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-12-26 16:54:41 +0000

    Add new user

--- dependancy-injection-rxjs-services/dep-injection-2/dependancy-injection-demo/src/app/app.component.ts
+++ dependancy-injection-rxjs-services/dep-injection-2/dependancy-injection-demo/src/app/app.component.ts
@@ -1,18 +1,25 @@
 import { Component } from '@angular/core';
-
+import { User } from './types/User';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Dependancy Injection 2';
 
-  users = [
-    {name: 'Ivan', age: 22},
-    {name: 'Mitko', age: 12},
-    {name: 'Pesho', age: 32},
-    {name: 'Penka', age: 82},
+  users: User[] = [
+    { name: 'Ivan', age: 22 },
+    { name: 'Mitko', age: 12 },
+    { name: 'Pesho', age: 32 },
+    { name: 'Penka', age: 82 },
   ];
-  
+
+  addUser(inpuName: HTMLInputElement, inputAge: HTMLInputElement): void {
+    const user = { name: inpuName.value, age: Number(inputAge.value) };
+    this.users.push(user);
+
+    inpuName.value = '';
+    inputAge.value = '';
+  }
 }
--- dependancy-injection-rxjs-services/dep-injection-2/dependancy-injection-demo/src/app/users-list/users-list.component.ts
+++ dependancy-injection-rxjs-services/dep-injection-2/dependancy-injection-demo/src/app/users-list/users-list.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { User } from '../types/User';
 @Component({
   selector: 'app-users-list',
   templateUrl: './users-list.component.html',
   styleUrls: ['./users-list.component.css']
 })
 export class UsersListComponent {
-
+@Input() users: User[] = [];
 }
