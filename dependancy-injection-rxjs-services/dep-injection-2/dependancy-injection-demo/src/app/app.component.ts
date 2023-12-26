43b7428b6c1f0ba4cd46bc04799c349c1729c32e
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-12-26 16:16:36 +0000

    Add users list component

--- dependancy-injection-rxjs-services/dep-injection-2/dependancy-injection-demo/src/app/app.component.ts
+++ dependancy-injection-rxjs-services/dep-injection-2/dependancy-injection-demo/src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Dependancy Injection 2';
+
+  users = [
+    {name: 'Ivan', age: 22},
+    {name: 'Mitko', age: 12},
+    {name: 'Pesho', age: 32},
+    {name: 'Penka', age: 82},
+  ];
+  
 }
