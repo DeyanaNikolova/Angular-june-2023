d21fcd1f87525b2ae27e5b320da2b403b70c516f
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-12-30 16:50:03 +0000

    Add loader to themes and posts lists

--- workshop-components/workshop-components/src/app/posts-list/posts-list.component.ts
+++ workshop-components/workshop-components/src/app/posts-list/posts-list.component.ts
@@ -5,15 +5,24 @@ import { Post } from '../interfaces/Post';
 @Component({
   selector: 'app-posts-list',
   templateUrl: './posts-list.component.html',
-  styleUrls: ['./posts-list.component.css']
+  styleUrls: ['./posts-list.component.css'],
 })
-export class PostsListComponent implements OnInit{
+export class PostsListComponent implements OnInit {
   posts: Post[] = [];
-constructor(private apiService: ApiService){}
-ngOnInit(): void {
-  this.apiService.getPosts(5).subscribe(posts =>{
-    console.log(posts); 
-    this.posts = posts;
-  });
-}
+  isLoading: boolean = true;
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.apiService.getPosts(5).subscribe({
+      next: (posts) => {
+        this.posts = posts;
+        this.isLoading = false;
+      },
+      error: (err) => {
+        this.isLoading = false;
+        console.error(`Error: ${err}`);
+      },
+    });
+  }
 }
--- workshop-components/workshop-components/src/app/themes-list/themes-list.component.ts
+++ workshop-components/workshop-components/src/app/themes-list/themes-list.component.ts
@@ -1,19 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
-import { Theme } from '../interfaces/Theme'; 
+import { Theme } from '../interfaces/Theme';
 
 @Component({
   selector: 'app-themes-list',
   templateUrl: './themes-list.component.html',
-  styleUrls: ['./themes-list.component.css']
+  styleUrls: ['./themes-list.component.css'],
 })
-export class ThemesListComponent implements OnInit{
-themes: Theme[] = [];
-constructor(private apiService: ApiService) {
-}
-ngOnInit(): void {
-  this.apiService.getThemes().subscribe(themes=>{
-    this.themes = themes;
-  });
-}
+export class ThemesListComponent implements OnInit {
+  themes: Theme[] = [];
+  isLoading: boolean = true;
+  constructor(private apiService: ApiService) {}
+  ngOnInit(): void {
+    this.apiService.getThemes().subscribe({
+      next: (themes) => {
+        this.themes = themes;
+        this.isLoading = false;
+      },
+      error: (err) => {
+        this.isLoading = false;
+        console.error(`Error: ${err}`);
+      },
+    });
+  }
 }
