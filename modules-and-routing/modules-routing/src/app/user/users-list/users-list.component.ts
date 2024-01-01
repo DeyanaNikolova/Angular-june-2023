cedb7f4fd5325ad12fcd53729661219326f205f2
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-01-01 14:11:21 +0000

    Show global loader conditionally

--- modules-and-routing/modules-routing/src/app/user/users-list/users-list.component.ts
+++ modules-and-routing/modules-routing/src/app/user/users-list/users-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
 import { User } from 'src/app/types/User';
+import { GlobalLoaderService } from 'src/app/core/global-loader/global-loader.service';
 
 @Component({
   selector: 'app-users-list',
@@ -9,11 +10,17 @@ import { User } from 'src/app/types/User';
 })
 export class UsersListComponent implements OnInit {
   userList: User[] = [];
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private globalLoaderService: GlobalLoaderService
+  ) {}
 
   ngOnInit(): void {
+    this.globalLoaderService.showLoader();
+    
     this.userService.fetchUsers().subscribe((users) => {
       this.userList = users;
+      this.globalLoaderService.hideLoader();
       console.log(users);
     });
   }
