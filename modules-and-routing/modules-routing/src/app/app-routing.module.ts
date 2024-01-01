a830a6fef5d309b34fd7b6d2c9e60827d9110907
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-01-01 11:09:05 +0000

    Clear app-routing module

--- modules-and-routing/modules-routing/src/app/app-routing.module.ts
+++ modules-and-routing/modules-routing/src/app/app-routing.module.ts
@@ -1,16 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TodoListComponent } from './todo/todo-list/todo-list.component';
-import { NotFoundComponent } from './core/not-found/not-found.component';
 
-const routes: Routes = [
-  {path: '', pathMatch: 'full', redirectTo: 'user/list'},
-  {path: 'todo-list', component: TodoListComponent},
-//  {path: '**', component: NotFoundComponent}
-];
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
