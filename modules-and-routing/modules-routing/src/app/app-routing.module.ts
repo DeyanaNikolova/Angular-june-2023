aae59c2d6543966e28119f9154e165ac636537a2
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-01-01 16:03:33 +0000

    Add routes to users and todos lists

--- modules-and-routing/modules-routing/src/app/app-routing.module.ts
+++ modules-and-routing/modules-routing/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsersListComponent } from './user/users-list/users-list.component';
+import { TodoListComponent } from './todo/todo-list/todo-list.component';
 
-
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: UsersListComponent },
+  { path: 'users-list', component: UsersListComponent },
+  {
+    path: 'todo-list',
+    component: TodoListComponent,
+  },
+];
 
 @NgModule({
-  imports: [RouterModule],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
