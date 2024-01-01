5616792afb4198d5d38291f1310514462fd957c6
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-01-01 13:39:12 +0000

    Get users

--- modules-and-routing/modules-routing/src/app/user/users-list/users-list.component.ts
+++ modules-and-routing/modules-routing/src/app/user/users-list/users-list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+import { User } from 'src/app/types/User';
 
 @Component({
   selector: 'app-users-list',
   templateUrl: './users-list.component.html',
-  styleUrls: ['./users-list.component.css']
+  styleUrls: ['./users-list.component.css'],
 })
-export class UsersListComponent {
+export class UsersListComponent implements OnInit {
+  userList: User[] = [];
+  constructor(private userService: UserService) {}
 
+  ngOnInit(): void {
+    this.userService.fetchUsers().subscribe((users) => {
+      this.userList = users;
+      console.log(users);
+    });
+  }
 }
