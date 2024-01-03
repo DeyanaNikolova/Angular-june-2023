c0ae2fdc8305ef766e7086dff1a0dfd0a65ceb4a
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-01-03 17:37:48 +0000

    Add activatedRoute functionality to user-details

--- modules-and-routing/modules-routing/src/app/user/user-details/user-details.component.ts
+++ modules-and-routing/modules-routing/src/app/user/user-details/user-details.component.ts
@@ -5,18 +5,20 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
-  styleUrls: ['./user-details.component.css']
+  styleUrls: ['./user-details.component.css'],
 })
-export class UserDetailsComponent implements OnInit{
-
-  
-  constructor(private activetedRoute: ActivatedRoute) {
-    console.log('snapshot.data', this.activetedRoute.snapshot.data);
-    this.activetedRoute.params.subscribe((v)=> console.log('parans.subscribe', v)
-    )
+export class UserDetailsComponent implements OnInit {
+  constructor(
+    private activetedRoute: ActivatedRoute,
+    private userService: UserService
+  ) {
+    console.log('snapshot.data', this.activetedRoute.snapshot.data['user']);
+    this.activetedRoute.params.subscribe((v) =>
+      console.log('parans.subscribe', v)
+    );
   }
 
   ngOnInit(): void {
-    
+ 
   }
 }
