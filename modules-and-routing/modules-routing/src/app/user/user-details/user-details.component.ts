72916e1047b18451ad30cc3984320a709a911f52
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-01-27 15:46:32 +0000

    Get user details

--- modules-and-routing/modules-routing/src/app/user/user-details/user-details.component.ts
+++ modules-and-routing/modules-routing/src/app/user/user-details/user-details.component.ts
@@ -1,18 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+import { User } from 'src/app/types/User';
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
   styleUrls: ['./user-details.component.css'],
 })
-export class UserDetailsComponent {
+export class UserDetailsComponent implements OnInit {
+  user: User;
   constructor(private activetedRoute: ActivatedRoute) {
+    this.user = this.activetedRoute.snapshot.data['user'];
     // static way
     console.log('snapshot.data', this.activetedRoute.snapshot.data['user']);
     // dynamic way
     this.activetedRoute.params.subscribe((v) =>
       console.log('params.subscribe', v)
     );
+  }
+  ngOnInit(): void {
+    this.user;
   }
 }
