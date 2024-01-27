2cac12217ff156645cd08e42ee4e4ba6a71d6836
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-01-27 12:21:23 +0000

    Update user-details.component.ts

--- modules-and-routing/modules-routing/src/app/user/user-details/user-details.component.ts
+++ modules-and-routing/modules-routing/src/app/user/user-details/user-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { User } from 'src/app/types/User';
+
 
 @Component({
   selector: 'app-user-details',
