1be3ab3699cf136bcbbe2d62d630e43c235f7618
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-01-27 17:41:28 +0000

    Export UesrRouthingModule

--- workshop-modules-and-routing/workshop-components/src/app/user/user.module.ts
+++ workshop-modules-and-routing/workshop-components/src/app/user/user.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
+import { UserRoutingModule } from './user-routing.module';
 
 
 
@@ -13,7 +14,9 @@ import { ProfileComponent } from './profile/profile.component';
     ProfileComponent
   ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    UserRoutingModule
+  ],
+  exports: [UserRoutingModule]
 })
 export class UserModule { }
