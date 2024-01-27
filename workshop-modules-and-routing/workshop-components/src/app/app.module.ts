09795e4f18cc442b397983466edf5b471ef1448b
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-01-27 17:42:06 +0000

    Import UserModule into app.module

--- workshop-modules-and-routing/workshop-components/src/app/app.module.ts
+++ workshop-modules-and-routing/workshop-components/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { PostsListComponent } from './posts-list/posts-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { SharedModule } from './shared/shared.module';
 import { HomeComponent } from './home/home.component';
+import { UserModule } from './user/user.module';
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,7 +24,8 @@ import { HomeComponent } from './home/home.component';
     AppRoutingModule,
     HttpClientModule,
     CoreModule,
-    SharedModule
+    SharedModule,
+    UserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
