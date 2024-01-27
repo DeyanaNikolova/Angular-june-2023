202258332ece183911a8dedd1b1dd607f003d5eb
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-01-27 12:12:01 +0000

    Update app-routing.module.ts

--- modules-and-routing/modules-routing/src/app/app-routing.module.ts
+++ modules-and-routing/modules-routing/src/app/app-routing.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { UsersListComponent } from './user/users-list/users-list.component';
 import { TodoListComponent } from './todo/todo-list/todo-list.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo:  'users/list' },
+  { path: '', pathMatch: 'full', redirectTo: 'users/list' },
   { path: 'todo-list', component: TodoListComponent },
 ];
 
