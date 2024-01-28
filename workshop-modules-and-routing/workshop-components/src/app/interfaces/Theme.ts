f53575484cd90d186a1e93c3e013157f53225b15
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-01-28 10:21:05 +0000

    Update Theme.ts

--- workshop-modules-and-routing/workshop-components/src/app/interfaces/Theme.ts
+++ workshop-modules-and-routing/workshop-components/src/app/interfaces/Theme.ts
@@ -1,10 +1,10 @@
-import { UserId } from "./UserId";
+import { User } from "./User";
 export interface Theme{
 subscribers: string[];
 posts: string[];
 _id: string;
 themeName: string;
-userId: UserId;
+userId: User;
 created_at: string;
 updatedAt: string;
 __v: number;
