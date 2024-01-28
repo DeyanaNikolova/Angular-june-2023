88bfc2420e7122771a990fb8cdfa623b7d62be56
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-01-28 10:40:11 +0000

    Update User.ts

--- workshop-modules-and-routing/workshop-components/src/app/interfaces/User.ts
+++ workshop-modules-and-routing/workshop-components/src/app/interfaces/User.ts
@@ -1,12 +1,13 @@
 export interface User {
-    _id: string;
-    themes: string[];
-    posts: string[];
-    tel: string;
+   // _id: string;
+    // themes: string[];
+    // posts: string[];
+    // tel: string;
     email: string;
-    username: string;
-    password: string;
-    created_at: string;
-    updatedAt: string;
-    __v: number;
+    // username: string;
+    // password: string;
+    // created_at: string;
+    // updatedAt: string;
+    // __v: number;
+    firstName: string;
 }
