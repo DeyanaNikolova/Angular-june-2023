dea18407346b84f7d9ed6ed644e512f9e4c6e0ac
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-01-28 10:22:16 +0000

    Create user service

---
+++ workshop-modules-and-routing/workshop-components/src/app/user/user.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { User } from '../interfaces/User';
+
+const USER_KEY = '[user]';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  user: User | undefined;
+
+  constructor() {
+    try {
+      const lsUser = localStorage.getItem(USER_KEY) || '';
+      this.user = JSON.parse(lsUser);
+    } catch (error) {
+      this.user = undefined;
+    }
+  }
+}
