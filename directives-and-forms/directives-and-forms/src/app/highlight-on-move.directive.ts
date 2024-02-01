669662371f75362f2d5bea636b3c08268d9278d7
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-02-01 15:24:10 +0000

    Add styles using renderer dynamically

--- directives-and-forms/directives-and-forms/src/app/highlight-on-move.directive.ts
+++ directives-and-forms/directives-and-forms/src/app/highlight-on-move.directive.ts
@@ -1,24 +1,50 @@
 import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
-  selector: '[appHighlightOnMove]'
+  selector: '[appHighlightOnMove]',
 })
 export class HighlightOnMoveDirective implements OnInit {
-
-  constructor(
-    private elRef: ElementRef,
-    private renderer: Renderer2
-    ) { }
+  constructor(private elRef: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit(): void {
     console.log(this.elRef);
 
-   // this.elRef.nativeElement.style.backgroundColor = 'yellow';
+    // Hard-codded
+    // this.elRef.nativeElement.style.backgroundColor = 'yellow';
 
-    this.renderer.setStyle(
+    // Using Renderer2
+    // this.renderer.setStyle(
+    //   this.elRef.nativeElement,
+    //   'background-color',
+    //   'red'
+    // );
+    this.renderer.listen(
       this.elRef.nativeElement,
-      'background-color',
-      'red'
+      'mouseenter',
+      this.mouseEnterHandler.bind(this)
     );
+
+    this.renderer.listen(
+      this.elRef.nativeElement,
+      'mouseleave',
+      this.mouseLeaveHandler.bind(this)
+    );
+  }
+  mouseEnterHandler(e: MouseEvent): void {
+    // this.renderer.setStyle(
+    //   this.elRef.nativeElement,
+    //   'background-color',
+    //   'yellow'
+    // );
+    this.renderer.addClass(this.elRef.nativeElement, 'highlight')
+  }
+
+  mouseLeaveHandler(e: MouseEvent): void {
+    // this.renderer.setStyle(
+    //   this.elRef.nativeElement,
+    //   'background-color',
+    //   'white'
+    // );
+    this.renderer.removeClass(this.elRef.nativeElement, 'highlight')
   }
 }
