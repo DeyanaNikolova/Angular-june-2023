69f1456765c71f381eed73181793692a9198e4cb
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-02-03 14:21:15 +0000

    Build register form

--- directives-and-forms/directives-and-forms/src/app/register/register.component.ts
+++ directives-and-forms/directives-and-forms/src/app/register/register.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
+  registerForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(10)]]
+  })
 
+  constructor(private fb: FormBuilder){}
+
+  ngOnInit(): void {
+    
+  }
+
+  submitHandler(): void{
+
+  }
 }
