c528cf5a74263e80d3be4489ac3c7913e8fb5b17
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-02-03 13:09:03 +0000

    Add login submit handler

--- directives-and-forms/directives-and-forms/src/app/login/login.component.ts
+++ directives-and-forms/directives-and-forms/src/app/login/login.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  // Another way to get login form without passing it as a parameter of the callback
+  // @ViewChild('loginForm') loginForm: NgForm | undefined;
 
+
+  // will rendere only tyhe static content
+ngOnInit(): void {
+  // this.loginForm.valueChanges.subscribe()
+}
+
+// ngAfterViewInit(): void {
+//    will render the final content with dynamic components
+// }
+
+  submitHandler(form: NgForm): void {
+    if (form.invalid) {
+      return;
+    }
+
+    const value: { email: string; password: string } = form.value;
+    console.log(value.password);
+    console.log(value);
+    
+    form.setValue({ email: '', password: '' });
+  }
 }
