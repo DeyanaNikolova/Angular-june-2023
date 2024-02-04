ac1d2485ef3bcd4d6be2710effa24dd0eae84144
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-02-04 13:30:24 +0000

    Add logic to email directive

--- workshop-forms/workshop-modules-and-routing/workshop-components/src/app/shared/validators/app-email.directive.ts
+++ workshop-forms/workshop-modules-and-routing/workshop-components/src/app/shared/validators/app-email.directive.ts
@@ -1,10 +1,29 @@
-import { Directive } from '@angular/core';
-
+import { Directive, Input, OnChanges, SimpleChanges } from '@angular/core';
+import {
+  AbstractControl,
+  ValidationErrors,
+  Validator,
+  ValidatorFn,
+} from '@angular/forms';
+import { appEmailValidator } from './app-email.validator';
 @Directive({
-  selector: '[appAppEmail]'
+  selector: '[appEmail]',
 })
-export class AppEmailDirective {
+export class AppEmailDirective implements Validator, OnChanges {
+  @Input() appEmail: string[] = [];
 
-  constructor() { }
+  validator: ValidatorFn = () => null;
 
+  constructor() {}
+
+  validate(control: AbstractControl<any, any>): ValidationErrors | null {
+    return this.validator(control);
+  }
+  
+  ngOnChanges(changes: SimpleChanges): void {
+    const currentChanges = changes['appEmail'];
+    if (currentChanges) {
+      this.validator = appEmailValidator(currentChanges.currentValue);
+    }
+  }
 }
