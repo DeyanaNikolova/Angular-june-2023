4a112f6b68df626c4a3dcb75a28d6167de6abd1d
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-02-04 14:34:17 +0000

    Check if form is invalid

--- workshop-forms/workshop-modules-and-routing/workshop-components/src/app/user/login/login.component.ts
+++ workshop-forms/workshop-modules-and-routing/workshop-components/src/app/user/login/login.component.ts
@@ -14,9 +14,11 @@ export class LoginComponent {
   constructor(private userService: UserService, private router: Router) {}
 
   login(form: NgForm): void {
-    console.log(form.value);
+    if(form.invalid){
+      return;
+    }
     
-    // this.userService.login();
-    // this.router.navigate(['/']);
+    this.userService.login();
+    this.router.navigate(['/']);
   }
 }
