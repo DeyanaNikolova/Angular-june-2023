ebbb6ad65eaf67c7a2802dd307380479db39ce98
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-02-04 11:23:43 +0000

    Import shared module

--- workshop-forms/workshop-modules-and-routing/workshop-components/src/app/user/user.module.ts
+++ workshop-forms/workshop-modules-and-routing/workshop-components/src/app/user/user.module.ts
@@ -5,10 +5,11 @@ import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
 import { UserRoutingModule } from './user-routing.module';
 import { FormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent, ProfileComponent],
-  imports: [CommonModule, UserRoutingModule, FormsModule],
+  imports: [CommonModule, UserRoutingModule, FormsModule, SharedModule],
   exports: [UserRoutingModule],
 })
 export class UserModule {}
