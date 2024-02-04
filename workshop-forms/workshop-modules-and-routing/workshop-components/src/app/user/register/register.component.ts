c0ce039098c6ff62e5e3335b1a437155c8e938f4
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-02-04 15:20:46 +0000

    Build reactive form for register

--- workshop-forms/workshop-modules-and-routing/workshop-components/src/app/user/register/register.component.ts
+++ workshop-forms/workshop-modules-and-routing/workshop-components/src/app/user/register/register.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  form = this.fb.group({
+    email: [''],
+    username: [''],
+    tel: [''],
+    passGroup: this.fb.group({
+      password: [''],
+      rePassword: [''],
+    }),
+  });
 
+  constructor(private fb: FormBuilder) {}
+
+  register():void{
+    console.log(this.form.value);
+    
+  }
 }
