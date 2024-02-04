19f7755633e771833d5587ad4b07bf6fa7023e86
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-02-04 10:32:14 +0000

    Add form to login functionality

--- workshop-forms/workshop-modules-and-routing/workshop-components/src/app/user/login/login.component.ts
+++ workshop-forms/workshop-modules-and-routing/workshop-components/src/app/user/login/login.component.ts
@@ -1,21 +1,20 @@
 import { Component } from '@angular/core';
 import { UserService } from '../user.service';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  constructor(private userService: UserService, private router: Router) {}
 
-  constructor(
-    private userService: UserService,
-    private router: Router
-    ) {}
-
-  login(email: string, password: string): void{
-this.userService.login();
-this.router.navigate(['/'])
+  login(form: NgForm): void {
+    console.log(form.value);
+    
+    this.userService.login();
+    this.router.navigate(['/']);
   }
 }
