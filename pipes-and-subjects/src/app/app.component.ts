231421006abaa09e298a9481ecb3d126c2b3d323
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-02-17 16:00:23 +0000

    Add reduce pipe

--- pipes-and-subjects/src/app/app.component.ts
+++ pipes-and-subjects/src/app/app.component.ts
@@ -3,6 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  user = {
+    name: 'Pesho',
+    age: 12,
+    list: [1, 2, 3, 4, 5, 6, 7, 8],
+  };
+
+  sum(a: number, b: number): number {
+    return a + b;
+  }
+
+  addProperty(): void {
+    (this.user as any)['test123'] = 'test123';
+
+    this.user.list = [...this.user.list, 100];
+    console.log(this.user);
+  }
+
+  p = new Promise((resolve) => {
+    resolve(100);
+  });
+}
---
+++ pipes-and-subjects/src/app/reduce.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'reduce',
+  // pure: false -> not pure functions will re-render
+  // pure: true -> pure functions will be memoized - DEFAULT
+})
+export class ReducePipe<T> implements PipeTransform {
+  transform(
+    array: T[],
+    reduceFn: (acc: any, current: T) => any,
+    initialValue: T
+  ): unknown {
+    // const sum = (acc, curr)=> acc + curr;
+    // [1, 2, 3].reduce(sum, 0)
+    console.log('Invoced from ReducePipe');
+    
+    return array.reduce(reduceFn, initialValue);
+  }
+}
