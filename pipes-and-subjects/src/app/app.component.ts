8555466c83fc07b5fae213bd64dc295f8fa064d6
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-02-18 12:37:11 +0000

    Get users with  ngOnInit

--- pipes-and-subjects/src/app/app.component.ts
+++ pipes-and-subjects/src/app/app.component.ts
@@ -1,17 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { interval, map } from 'rxjs';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   user = {
     name: 'Pesho',
     age: 12,
     list: [1, 2, 3, 4, 5, 6, 7, 8],
   };
+
+  
+  constructor(private userService: UserService) {}
 
   sum(a: number, b: number): number {
     return a + b;
@@ -31,4 +35,10 @@ export class AppComponent {
   });
 
   time$ = interval(1000).pipe(map(() => new Date()));
+ngOnInit(): void {
+  this.userService.loadUsers().subscribe({
+    next: console.log,
+    
+  })
+}
 }
