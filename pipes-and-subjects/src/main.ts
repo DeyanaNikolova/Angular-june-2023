02f89009c4e6d752c92611b5b572d812566c897f
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-02-18 15:28:04 +0000

    Add behaviour subject demo

--- pipes-and-subjects/src/main.ts
+++ pipes-and-subjects/src/main.ts
@@ -1,14 +1,14 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import { Observable, Subject } from 'rxjs';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 import { map } from 'rxjs';
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
 
-
-  // Promise Demo 
+// Promise Demo
 //   const p = new Promise(resolve => {
 //     resolve(100);
 //   });
@@ -16,7 +16,6 @@ platformBrowserDynamic().bootstrapModule(AppModule)
 //   p.then(data=>{
 //     console.log(data);
 //   });
-
 
 // Observable Demo
 //   const o$ = new Observable<number>((obs)=>{
@@ -60,20 +59,32 @@ platformBrowserDynamic().bootstrapModule(AppModule)
 //   complete: ()=> console.log('Obs completed!'),
 // });
 
-
 // setTimeout(() => {
 //   subscribtion.unsubscribe();
 // }, 3000);
 
 // Subject Demo
-const subj$$ = new Subject();
-subj$$.subscribe(console.log);
-subj$$.next(123);
-subj$$.subscribe(console.log);
-subj$$.subscribe(console.log);
-subj$$.next(400);
+// const subj$$ = new Subject();
+// subj$$.subscribe(console.log);
+// subj$$.next(123);
+// subj$$.subscribe(console.log);
+// subj$$.subscribe(console.log);
+// subj$$.next(400);
 // subj$$.subscribe({
 //   next: console.log,
 //   error: (err)=>console.error(`Error: ${err}`),
 //   complete: ()=> console.log('Obs completed!'),
 // })
+
+// Behavior Sunbject
+const bSubj$$ = new BehaviorSubject(100);
+bSubj$$.subscribe((data)=> console.log('Subscription 1: ', data));
+
+setTimeout(() => {
+  bSubj$$.next(200);
+  bSubj$$.subscribe((data)=> console.log('Subscription 2: ', data));
+  setTimeout(() => {
+    bSubj$$.next(300);
+    bSubj$$.subscribe((data)=> console.log('Subscription 3: ', data));
+  }, 2000);
+}, 2000);
