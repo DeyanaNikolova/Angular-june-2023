f962a61ef1e00f5d7efce3793a4aa1ec9e7f92c8
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-02-18 14:45:06 +0000

    Update main.ts

--- pipes-and-subjects/src/main.ts
+++ pipes-and-subjects/src/main.ts
@@ -8,29 +8,31 @@ platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
 
 
-  // Promise Demo
-  const p = new Promise(resolve => {
-    resolve(100);
-  });
+  // Promise Demo 
+//   const p = new Promise(resolve => {
+//     resolve(100);
+//   });
 
-  p.then(data=>{
-    console.log(data);
-  });
+//   p.then(data=>{
+//     console.log(data);
+//   });
 
-  const o$ = new Observable<number>((obs)=>{
-    obs.next(1000);
-    obs.next(2000);
-    obs.next(3000);
 
-    obs.error(new Error('bad error'));
+// Observable/Subject Demo
+//   const o$ = new Observable<number>((obs)=>{
+//     obs.next(1000);
+//     obs.next(2000);
+//     obs.next(3000);
 
-    return ()=>{
-      // cleanup
-    }
-  });
+//     obs.error(new Error('bad error'));
 
-o$.pipe(map((num: number) => num + 1)).subscribe({
-  next: console.log,
-  error: (err)=> console.error(`Error: ${err}`),
-  complete: ()=>console.log('Obs completed!')
-})
+//     return ()=>{
+//       // cleanup
+//     }
+//   });
+
+// o$.pipe(map((num: number) => num + 1)).subscribe({
+//   next: console.log,
+//   error: (err)=> console.error(`Error: ${err}`),
+//   complete: ()=>console.log('Obs completed!')
+// })
