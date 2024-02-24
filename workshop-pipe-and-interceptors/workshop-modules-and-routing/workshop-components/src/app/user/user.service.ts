d17c568b53f3c08094237b1280b9c18ad8c6e16d
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-02-24 16:19:31 +0000

    Make api call with post request for login and register

--- workshop-pipe-and-interceptors/workshop-modules-and-routing/workshop-components/src/app/user/user.service.ts
+++ workshop-pipe-and-interceptors/workshop-modules-and-routing/workshop-components/src/app/user/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { User } from '../interfaces/User';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
@@ -7,11 +8,11 @@ import { User } from '../interfaces/User';
 export class UserService {
   user: User | undefined;
   USER_KEY = '[user]';
-  get isLogged(): boolean{
+  get isLogged(): boolean {
     return !!this.user;
   }
 
-  constructor() {
+  constructor(private http: HttpClient) {
     try {
       const lsUser = localStorage.getItem(this.USER_KEY) || '';
       this.user = JSON.parse(lsUser);
@@ -20,16 +21,21 @@ export class UserService {
     }
   }
 
-  login(): void{
-    this.user = {
-      email: 'john.doe@gmail.com',
-      firstName: 'John',
-    };
+  login(email: string, password: string) {
+    //   this.user = {
+    //     email: 'john.doe@gmail.com',
+    //     firstName: 'John',
+    //   };
 
-    localStorage.setItem(this.USER_KEY, JSON.stringify(this.user));
+    //   localStorage.setItem(this.USER_KEY, JSON.stringify(this.user));
+    return this.http.post('/api/login', { email, password });
   }
 
-  logout(): void{
+  register(username: string, email: string, password: string, rePassword: string, tel: string){
+    return this.http.post('/api/register', { username, email, password, rePassword, tel });
+  }
+
+  logout(): void {
     this.user = undefined;
     localStorage.removeItem(this.USER_KEY);
   }
