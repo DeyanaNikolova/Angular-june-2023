f8d3e5d3cd8a6e0bd419632b4db1ff93753d1a20
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-02-24 16:18:15 +0000

    Add register functionality

--- workshop-pipe-and-interceptors/workshop-modules-and-routing/workshop-components/src/app/user/register/register.component.ts
+++ workshop-pipe-and-interceptors/workshop-modules-and-routing/workshop-components/src/app/user/register/register.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { DEFAULT_EMAIL_DOMAINS } from 'src/app/shared/constants';
 import { appEmailValidator } from 'src/app/shared/validators/app-email.validator';
-import { matchPasswordsValidator } from 'src/app/shared/validators/match-passwords.validator'; 
+import { matchPasswordsValidator } from 'src/app/shared/validators/match-passwords.validator';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -11,23 +13,43 @@ import { matchPasswordsValidator } from 'src/app/shared/validators/match-passwor
 export class RegisterComponent {
   form = this.fb.group({
     username: ['', [Validators.required, Validators.minLength(5)]],
-    email: ['', [Validators.required, appEmailValidator(DEFAULT_EMAIL_DOMAINS)]],
+    email: [
+      '',
+      [Validators.required, appEmailValidator(DEFAULT_EMAIL_DOMAINS)],
+    ],
     tel: [''],
-    passGroup: this.fb.group({
-      password: ['', [Validators.required, Validators.minLength(5)]],
-      rePassword: ['', [Validators.required, Validators.minLength(5)]],
-    },
-    {
-      validators: [matchPasswordsValidator('password', 'rePassword')]
-    }),
+    passGroup: this.fb.group(
+      {
+        password: ['', [Validators.required, Validators.minLength(5)]],
+        rePassword: ['', [Validators.required, Validators.minLength(5)]],
+      },
+      {
+        validators: [matchPasswordsValidator('password', 'rePassword')],
+      }
+    ),
   });
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private userService: UserService,
+    private router: Router
+  ) {}
 
-  register():void{
-    if(this.form.invalid){
+  register(): void {
+    if (this.form.invalid) {
       return;
     }
-    console.log(this.form.value);
+    const {
+      username,
+      email,
+      passGroup: { password, rePassword } = {},
+      tel,
+    } = this.form.value;
+
+    this.userService
+      .register(username!, email!, password!, rePassword!, tel!)
+      .subscribe(() => {
+        this.router.navigate(['/login']);
+      });
   }
 }
