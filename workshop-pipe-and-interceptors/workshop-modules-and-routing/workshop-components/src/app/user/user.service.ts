719a59b48d51e7c48ec2783562fb47de0bdabac8
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-02-24 17:17:23 +0000

    Add subject and observable to handle user data

--- workshop-pipe-and-interceptors/workshop-modules-and-routing/workshop-components/src/app/user/user.service.ts
+++ workshop-pipe-and-interceptors/workshop-modules-and-routing/workshop-components/src/app/user/user.service.ts
@@ -1,34 +1,29 @@
 import { Injectable } from '@angular/core';
 import { User } from '../interfaces/User';
 import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
+  private user$$ = new BehaviorSubject<User | undefined>(undefined);
+  public user$ = this.user$$.asObservable();
+
   user: User | undefined;
+
+  constructor(private http: HttpClient) {}
+
   USER_KEY = '[user]';
   get isLogged(): boolean {
     return !!this.user;
   }
 
-  constructor(private http: HttpClient) {
-    try {
-      const lsUser = localStorage.getItem(this.USER_KEY) || '';
-      this.user = JSON.parse(lsUser);
-    } catch (error) {
-      this.user = undefined;
-    }
-  }
 
   login(email: string, password: string) {
-    //   this.user = {
-    //     email: 'john.doe@gmail.com',
-    //     firstName: 'John',
-    //   };
-
-    //   localStorage.setItem(this.USER_KEY, JSON.stringify(this.user));
-    return this.http.post('/api/login', { email, password });
+    return this.http
+      .post<User>('/api/login', { email, password })
+      .pipe(tap((user) => this.user$$.next(user)));
   }
 
   register(
@@ -38,17 +33,19 @@ export class UserService {
     rePassword: string,
     tel: string
   ) {
-    return this.http.post('/api/register', {
+    return this.http.post<User>('/api/register', {
       username,
       email,
       password,
       rePassword,
       tel,
-    });
+    })
+    .pipe(tap((user) => this.user$$.next(user)));
   }
 
-  logout(): void {
-    this.user = undefined;
-    localStorage.removeItem(this.USER_KEY);
+  logout() {
+    return this.http
+    .post<User>('/api/logout', {})
+    .pipe(tap(() => this.user$$.next(undefined)));
   }
 }
