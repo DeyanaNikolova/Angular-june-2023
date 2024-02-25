843541019dcb793223ae118bcf307b4a87a3ac2d
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-02-25 11:34:17 +0000

    Create authenticate component and wrap the app in it  adding condition for authentication

--- workshop-pipe-and-interceptors/workshop-modules-and-routing/workshop-components/src/app/app.module.ts
+++ workshop-pipe-and-interceptors/workshop-modules-and-routing/workshop-components/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ThemeModule } from './theme/theme.module';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { appInterceptorProvider } from './app.interceptor';
+import { AuthenticateComponent } from './authenticate/authenticate.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,6 +24,7 @@ import { appInterceptorProvider } from './app.interceptor';
     PostsListComponent,
     WelcomeComponent,
     NotFoundComponent,
+    AuthenticateComponent,
   ],
   imports: [
     BrowserModule,
