863ee9b22bc4af3c566e55cc67b431ace8e690ac
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-02-25 13:11:27 +0000

    Update error.component.ts

--- workshop-pipe-and-interceptors/workshop-modules-and-routing/workshop-components/src/app/core/error/error.component.ts
+++ workshop-pipe-and-interceptors/workshop-modules-and-routing/workshop-components/src/app/core/error/error.component.ts
@@ -6,13 +6,13 @@ import { ErrorService } from './error.service';
   templateUrl: './error.component.html',
   styleUrls: ['./error.component.css'],
 })
-export class ErrorComponent implements OnInit{
+export class ErrorComponent implements OnInit {
   apiError$ = this.errorService.apiError$$.asObservable();
   errorMsg: string = '';
   constructor(private errorService: ErrorService) {}
   ngOnInit(): void {
-    this.apiError$.subscribe((err: any)=>{
+    this.apiError$.subscribe((err: any) => {
       this.errorMsg = err.message;
-    })
+    });
   }
 }
