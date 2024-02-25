93e9d6b8d4da4ec9e97cc72b8744f121e43806d5
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-02-25 12:43:51 +0000

    Add error handling and show error message

--- workshop-pipe-and-interceptors/workshop-modules-and-routing/workshop-components/src/app/app.interceptor.ts
+++ workshop-pipe-and-interceptors/workshop-modules-and-routing/workshop-components/src/app/app.interceptor.ts
@@ -6,24 +6,39 @@ import {
   HttpInterceptor,
   HttpRequest,
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError } from 'rxjs';
 import { environment } from 'src/environments/environment.development';
+import { ErrorService } from './core/error/error.service';
+import { Router } from '@angular/router';
 
 const { apiUrl } = environment;
 
 @Injectable()
 export class AppInterceptor implements HttpInterceptor {
+
+  constructor(private errorService: ErrorService, private router: Router) {}
+
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    if(req.url.startsWith('/api')){
-        req = req.clone({
-            url: req.url.replace('/api', apiUrl),
-            withCredentials: true,
-        })
+    if (req.url.startsWith('/api')) {
+      req = req.clone({
+        url: req.url.replace('/api', apiUrl),
+        withCredentials: true,
+      });
     }
-    return next.handle(req);
+    return next.handle(req).pipe(
+      catchError(err =>{
+        this.errorService.setError(err);
+        if(err.status === 401){
+          this.router.navigate(['/auth/login']);
+        }else{
+          this.router.navigate(['/error']);
+        }
+        return [err];
+      })
+    )
   }
 }
 
