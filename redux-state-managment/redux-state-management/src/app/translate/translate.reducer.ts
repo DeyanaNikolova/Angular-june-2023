0944b81f2ba79ce221499c7309713ded1d77f5e6
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-03-03 13:53:39 +0000

    Update translate.reducer.ts

--- redux-state-managment/redux-state-management/src/app/translate/translate.reducer.ts
+++ redux-state-managment/redux-state-management/src/app/translate/translate.reducer.ts
@@ -1,7 +1,6 @@
-import { Action } from "@ngrx/store";
-
+import { Action } from '@ngrx/store';
 
 export function translateReducer(state: string = 'Hello!', action: Action) {
-    console.log(action.type, state);
-    return state;
+  console.log(action.type, state);
+  return state;
 }
