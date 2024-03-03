16e53b05c6be334bfbe9ed323ce7ac6e3b883587
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-03-03 13:31:40 +0000

    Create translate component

--- redux-state-managment/redux-state-management/src/app/app.module.ts
+++ redux-state-managment/redux-state-management/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReduxSimulationComponent } from './redux-simulation/redux-simulation.component';
+import { TranslateComponent } from './translate/translate.component';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ReduxSimulationComponent
+    ReduxSimulationComponent,
+    TranslateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
