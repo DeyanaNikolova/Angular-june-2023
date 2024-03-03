bebace58e6eb3cb5f24a8cd2df46bd9e78779858
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-03-03 17:17:03 +0000

    Create post.actions.ts

---
+++ redux-state-managment/redux-state-management/src/app/post/post.actions.ts
@@ -0,0 +1,29 @@
+import { Action } from "@ngrx/store";
+
+// Define Types
+export const EDIT_TEXT = '[Post] Edit';
+export const RESET = '[Post] Reset';
+export const UPVOTE = '[Post] Upvote';
+export const DOWNVOTE = '[Post] Downvote';
+
+
+// Define Actions
+export class EditText implements Action {
+    readonly type = EDIT_TEXT;
+
+    constructor(payload: string){}
+}
+
+export class Reset implements Action{
+    readonly type = RESET;
+}
+
+export class Upvote implements Action{
+    readonly type = UPVOTE;
+}
+
+export class Downvote implements Action{
+    readonly type = DOWNVOTE;
+}
+
+export type All = Upvote | Downvote | Reset | EditText;
