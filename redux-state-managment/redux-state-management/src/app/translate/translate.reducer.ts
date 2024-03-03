e2697e586bd65442cd7d95a195aa061fd517433c
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-03-03 14:28:14 +0000

    Add action types

--- redux-state-managment/redux-state-management/src/app/translate/translate.reducer.ts
+++ redux-state-managment/redux-state-management/src/app/translate/translate.reducer.ts
@@ -2,5 +2,17 @@ import { Action } from '@ngrx/store';
 
 export function translateReducer(state: string = 'Hello!', action: Action) {
   console.log(action.type, state);
-  return state;
+
+  switch (action.type) {
+    case 'BULGARIAN':
+      return (state = 'Здрасти!');
+    case 'ENGLISH':
+      return (state = 'Hello!');
+    case 'DEUTSCH':
+      return (state = 'Guten Taag!');
+      case 'ITALIAN':
+        return (state = 'Buongiorno!');
+    default:
+      return state;
+  }
 }
