f9957800f4d069b75bc4eeea9fb69e669e01816b
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-06-16 13:58:43 +0100

    Add image url

--- fetch-data-angular-17/fetch-data-angular-17/src/environments/environment.ts
+++ fetch-data-angular-17/fetch-data-angular-17/src/environments/environment.ts
@@ -2,4 +2,5 @@ export const environment = {
     production: true,
     apiKey: '0820c9615aaf3e5338918eabe92bb024',
     apiUrl: 'https:/api.themoviedb.org/3/discover/movie',
+    imgUrl: 'https://image.tmdb.org/t/p/w500',
 };
