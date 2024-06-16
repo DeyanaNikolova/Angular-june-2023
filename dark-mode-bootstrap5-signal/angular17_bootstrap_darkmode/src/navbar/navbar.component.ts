3db6f2490028a277cada81906a01fedb2e8da56d
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-06-16 15:56:04 +0100

    Inject darkmood service and add toggle functionality

--- dark-mode-bootstrap5-signal/angular17_bootstrap_darkmode/src/navbar/navbar.component.ts
+++ dark-mode-bootstrap5-signal/angular17_bootstrap_darkmode/src/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { DarkModeService } from '../app/services/dark-mode.service';
 
 @Component({
   selector: 'app-navbar',
@@ -8,5 +9,8 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+darkModeService: DarkModeService = inject(DarkModeService);
+toggleDarkMode(){
+  this.darkModeService.updateDarkMode();
+}
 }
