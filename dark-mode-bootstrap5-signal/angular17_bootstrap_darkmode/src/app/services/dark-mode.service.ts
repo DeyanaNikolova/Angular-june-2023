365c397049345a7bc9db1b8511115111a18dbc80
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-06-16 17:04:15 +0100

    Add darkmood signal to local storage

--- dark-mode-bootstrap5-signal/angular17_bootstrap_darkmode/src/app/services/dark-mode.service.ts
+++ dark-mode-bootstrap5-signal/angular17_bootstrap_darkmode/src/app/services/dark-mode.service.ts
@@ -1,12 +1,19 @@
-import { Injectable, signal } from '@angular/core';
+import { Injectable, signal, effect } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DarkModeService {
-darkModeSignal = signal<string>('null');
-  constructor() { }
+darkModeSignal = signal<string>(
+  JSON.parse(window.localStorage.getItem('darkModeSignal') ?? 'null')
+);
+ 
   updateDarkMode(){
     this.darkModeSignal.update((value)=>(value === "dark" ? "null": "dark"));
+  }
+  constructor() { 
+    effect(()=>{
+      window.localStorage.setItem('darkModeSingnal', JSON.stringify(this.darkModeSignal()));
+    })
   }
 }
