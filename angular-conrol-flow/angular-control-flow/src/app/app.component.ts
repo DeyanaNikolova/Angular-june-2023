bb56a0f66dda7ddcaa1f5d273e7912bf50a7856f
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-06-16 19:23:45 +0100

    Add product list

--- angular-conrol-flow/angular-control-flow/src/app/app.component.ts
+++ angular-conrol-flow/angular-control-flow/src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-control-flow';
+  title = 'Angular Control Flow';
+
+  userRole: string = 'candidate';
+
+  productList = [
+    {id:1, name: 'I Phone'},
+    {id:2, name: 'I Pad'},
+    {id:3, name: 'MacBook'},
+    {id:4, name: 'HP Laptop'},
+    {id:5, name: 'Samsung TV'},
+  ]
 }
