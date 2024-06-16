401ea6c7890eb90e9818b96074e57fd4d7fbcfc2
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-06-16 13:59:09 +0100

    Add movie component to app

--- fetch-data-angular-17/fetch-data-angular-17/src/app/app.component.ts
+++ fetch-data-angular-17/fetch-data-angular-17/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { PostComponent } from './post/post.component';
+import { MovieComponent } from './movie/movie.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, PostComponent],
+  imports: [CommonModule, RouterOutlet, PostComponent, MovieComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
