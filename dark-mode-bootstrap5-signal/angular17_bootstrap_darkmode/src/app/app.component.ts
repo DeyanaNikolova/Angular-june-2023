56ad19d0a200a54402553887d32aed64066778b3
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-06-16 16:11:52 +0100

    Add footer and content components

--- dark-mode-bootstrap5-signal/angular17_bootstrap_darkmode/src/app/app.component.ts
+++ dark-mode-bootstrap5-signal/angular17_bootstrap_darkmode/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { DarkModeService } from './services/dark-mode.service';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, ContentComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
