a97c04cd055891117b50d9a4cb2cf4cf6ba6cb9e
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-06-16 14:00:38 +0100

    Add load movies functionality

--- fetch-data-angular-17/fetch-data-angular-17/src/app/movie/movie.component.ts
+++ fetch-data-angular-17/fetch-data-angular-17/src/app/movie/movie.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { MovieService } from '../services/movie.service';
+import { MovieDetail } from '../interfaces/movie-detail';
+import { environment } from '../../environments/environment.development';
 
+const imgUrl = environment.imgUrl;
 @Component({
   selector: 'app-movie',
   standalone: true,
@@ -7,6 +11,23 @@ import { Component } from '@angular/core';
   templateUrl: './movie.component.html',
   styleUrl: './movie.component.css'
 })
-export class MovieComponent {
+export class MovieComponent implements OnInit{
+private movieService = inject(MovieService);
+movies: MovieDetail[] = [];
 
+ngOnInit(): void {
+  this.loadMovies();
+}
+loadMovies(){
+  this.movieService.getMovies().subscribe({
+    next: (res: any)=>{
+      this.movies = res.results as MovieDetail[];
+      console.log(res.results); 
+    },
+    error: (error)=>console.log('Error fetching movies: ' ,error)    
+  });
+}
+getFullImageUrl(posterPath: String): String{
+return imgUrl + posterPath;
+}
 }
