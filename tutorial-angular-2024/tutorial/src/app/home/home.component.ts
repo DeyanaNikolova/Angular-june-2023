17ca679e885460cc9f9fefdb93b5a2013ea353b2
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-09-14 17:38:43 +0100

    Get product output

--- tutorial-angular-2024/tutorial/src/app/home/home.component.ts
+++ tutorial-angular-2024/tutorial/src/app/home/home.component.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { ProductsService } from '../services/products.service';
+import { Component } from '@angular/core';
 import { Products, Product } from '../../types';
 import { ProductComponent } from "../components/product/product.component";
+import { ProductsService } from '../services/products.service';
 
 @Component({
   selector: 'app-home',
@@ -11,16 +11,23 @@ import { ProductComponent } from "../components/product/product.component";
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
   constructor(private productsService: ProductsService) {}
 
   products: Product[] = [];
+
+  
+  onProductOutput(product: Product){
+    console.log(product, 'Output');  
+  }
 
   ngOnInit(): void {
     this.productsService
       .getProducts('http://localhost:3000/clothes', { page: 0, perPage: 5 })
       .subscribe((products: Products) => {
         this.products = products.items;
+        console.log(products.items);
+        
       });
-  }
+  } 
 }
