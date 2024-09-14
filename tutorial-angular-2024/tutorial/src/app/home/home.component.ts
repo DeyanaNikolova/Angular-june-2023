e590a6e4bc2557f8952bc4987d614c1faa9f6fab
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-09-14 15:37:09 +0100

    Loop through products

--- tutorial-angular-2024/tutorial/src/app/home/home.component.ts
+++ tutorial-angular-2024/tutorial/src/app/home/home.component.ts
@@ -1,22 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ProductsService } from '../services/products.service';
-import { Products } from '../../types';
+import { Products, Product } from '../../types';
+import { ProductComponent } from "../components/product/product.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ProductComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent implements OnInit {
   constructor(private productsService: ProductsService) {}
 
+  products: Product[] = [];
+
   ngOnInit(): void {
     this.productsService
       .getProducts('http://localhost:3000/clothes', { page: 0, perPage: 5 })
       .subscribe((products: Products) => {
-        console.log(products.items);
+        this.products = products.items;
       });
   }
 }
