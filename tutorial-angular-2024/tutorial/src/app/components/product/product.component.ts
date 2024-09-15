71bcd30cd3bd8181088859150a014eb22deb7bc1
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-09-15 13:18:21 +0100

    Add confirmation service

--- tutorial-angular-2024/tutorial/src/app/components/product/product.component.ts
+++ tutorial-angular-2024/tutorial/src/app/components/product/product.component.ts
@@ -3,29 +3,39 @@ import { Product } from '../../../types';
 import { RatingModule } from 'primeng/rating';
 import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
-
-
+import { ConfirmationService } from 'primeng/api';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [ RatingModule, FormsModule, ButtonModule],
+  imports: [RatingModule, FormsModule, ButtonModule],
+  providers: [ConfirmationService],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss',
 })
 export class ProductComponent {
+  constructor(private confirmationService: ConfirmationService) {}
+
   @Input() product!: Product;
   @Output() edit: EventEmitter<Product> = new EventEmitter<Product>();
   @Output() delete: EventEmitter<Product> = new EventEmitter<Product>();
 
-  editProduct(){
+  editProduct() {
     this.edit.emit(this.product);
   }
 
-  deleteProduct(){
+  confirmDelete() {
+    this.confirmationService.confirm({
+      message: 'Are you sure that you want to delete this product?',
+      accept: () => {
+        this.deleteProduct();
+      },
+    });
+  }
+
+  deleteProduct() {
     this.delete.emit(this.product);
   }
 
-  ngOnInit(){}
-  
+  ngOnInit() {}
 }
