d43f42313c37f42ce7760c096d5cccbd7ef80002
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-09-21 16:26:35 +0100

    Add functionality markup and styles

--- airbnb-clone-fron/src/app/layout/navbar/category/category.component.ts
+++ airbnb-clone-fron/src/app/layout/navbar/category/category.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CategoryService } from './category.service';
+import { Category } from './category.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-category',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FontAwesomeModule],
   templateUrl: './category.component.html',
   styleUrl: './category.component.scss'
 })
-export class CategoryComponent {
+export class CategoryComponent implements OnInit{
+  categoryService: CategoryService = inject(CategoryService);
 
+  categories: Category[] | undefined;
+  currentActivatedCategory = this.categoryService.getCategoryByDefault();
+
+  ngOnInit(): void {
+    this.fetchCategories();
+  }
+
+  fetchCategories(){
+  this.categories =  this.categoryService.getCategories();
+  }
 }
