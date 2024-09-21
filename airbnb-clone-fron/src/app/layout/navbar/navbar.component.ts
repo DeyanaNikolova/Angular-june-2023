24a5e0f3ed2321e54a82aa9e8e1e174f1a238217
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-09-21 17:55:34 +0100

    Add markup and logic

--- airbnb-clone-fron/src/app/layout/navbar/navbar.component.ts
+++ airbnb-clone-fron/src/app/layout/navbar/navbar.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ToolbarModule } from 'primeng/toolbar';
+import { MenuModule } from 'primeng/menu';
+import { CategoryComponent } from './category/category.component';
+import { AvatarComponent } from './avatar/avatar.component';
+import { DialogService } from 'primeng/dynamicdialog';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    ButtonModule,
+    FontAwesomeModule,
+    ToolbarModule,
+    MenuModule,
+    CategoryComponent,
+    AvatarComponent,
+  ],
+  providers: [DialogService],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  location: string = 'Anywhere';
+  guests: string = 'Add guests';
+  dates: string = 'Any week';
 
+  //  login()=> this.authService.login();
+
+  //  logout()=> this.authService.logout();
+
+  currentMenuItems: MenuItem[] | undefined = [];
+
+  ngOnInit(): void {}
+
+  private fetchMenu() {
+    return [
+      {
+        label: 'Sign up',
+        styleClass: 'font-bold',
+      },
+      {
+        label: 'Log in',
+      },
+    ];
+  }
 }
